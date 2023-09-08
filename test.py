d427c8e387d761124c00db6a0a3879a64a38ce88
Anu Elizabath Shibu <ashibu@medicompinc.com>
2023-09-08 10:12:20 +0530

    added add and subtract functions

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 print("This is a test file.")
 print("-----------------------")
 print("+++++++++++++++++++++++")
+
+
+def add_numbers(a, b):
+    num = a + b
+    return num
+
+
+def subtract_numbers(a, b):
+    num = a - b
+    return num
